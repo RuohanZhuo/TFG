aee3c23273e4e838603c035b220dc7d52fd23bd9
RuohanZhuo <ruohan.zhuo@alumnos.upm.es>
2024-06-02 22:42:40 +0200

    Delete unused code

--- backend/app.js
+++ backend/app.js
@@ -26,21 +26,4 @@ app.use('/', timetableRouter);
 app.use('/', classroomRouter);
 app.use('/', subjectRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-
-  res.render('404');
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
 module.exports = app;
