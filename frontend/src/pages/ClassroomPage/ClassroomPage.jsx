5b41b9579042d5ce153d09abb1d60c266c5d502b
Ouzhan Lin <ouzhan.lin@alumnos.upm.es>
2024-06-16 14:12:45 +0200

    fix admin can see create class

--- frontend/src/pages/ClassroomPage/ClassroomPage.jsx
+++ frontend/src/pages/ClassroomPage/ClassroomPage.jsx
@@ -4,13 +4,16 @@ import ClassroomList from '../../components/ClassroomList'
 
 export default class Classroom extends Component {
   render() {
+
+    const rol = localStorage.getItem('rol')
+
     return (
       <div>
         <ClassroomList/>
         <div className="fixed-bottom text-center mb-3">
-          <NavLink className="btn btn-primary button_nav" to="/classroom/createClass">
+          {rol === 'professor' && <NavLink className="btn btn-primary button_nav" to="/classroom/createClass">
             Create Class
-          </NavLink>
+          </NavLink>}
         </div>
       </div>
     )
